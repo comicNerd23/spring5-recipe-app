ca9418375a1e10ac5e0da158b0daffd2ee2bee3c
comicnerd23 <m23pal@gmail.com>
2021-10-21 19:05:18 +0530

    Added Getter-Setter for Category

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -15,9 +15,27 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
-    public Category(Long id, String description, Set<Recipe> recipes) {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
         this.recipes = recipes;
     }
 }
